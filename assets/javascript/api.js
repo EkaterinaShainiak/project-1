a64650d6c3e2de2346a534c1bf3d094f95eaf8e5
Jay Yang <hjyang1@uci.edu>
2017-11-13 11:34:56 -0800

    Massive commit

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,1 +1,21 @@
+$(document).on("click", "#hello", function () {
+   
+    var action = "&action=job-stats";
+    var city = "&city=Irvine";
+    var jobType = "&jobType=fulltime";
+    
+    var webURL = "http://localhost:3000/data";
+       
+   
+    $.ajax({
+        url: webURL,
+        method: "GET"
+    })
+        .done(function (response) {
+            var results = JSON.parse(response);
+            console.log(results);
 
+        })
+
+
+})
