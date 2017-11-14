2b4d2c78a1790aa66e1a701ec77595298b400e56
Austin Okamuro <AustinOkamuro@gmail.com>
2017-11-14 14:01:56 -0800

    Storing character inputs in an object

--- assets/javascript/gameflow.js
+++ assets/javascript/gameflow.js
@@ -10,14 +10,30 @@ var characterQuestions = [{
     answers: ["White", "Not White", "Asian"]
 },  {
     question: "Select your gender:",
-    answers: ["Female", "Male", "Unknow"]
-}
-];
+    answers: ["Female", "Male", "Unknown"]
+}];
+
+// Character object
+var character = {
+    hairColor: "",
+    ethnicityChosen: "",
+    genderChosen: "",
+};
+
+var highschool = [{
+    question: "Life decision 1",
+    answers: true,
+}];
+
+var hair;
 
 // Narration 
 var nar1_5 = "This is what we want to narrate to player";
 
-var characterTraits;
+var hairChosen;
+var ethnicityChosen;
+var genderChosen;
+
 var game = {
 
     phase1: function()   {
@@ -29,15 +45,36 @@ var game = {
         for (var i = 0; i < characterQuestions.length; i++)  {
             panel.append("<h2>" + characterQuestions[i].question + "</h2>");
             for (var j = 0; j < characterQuestions[i].answers.length; j++)  {
-                panel.append("<input type='radio' name='question-" + i + " 'value=' " + characterQuestions[i].answers[j] + "'' class='checkbox'>" + characterQuestions[i].answers[j] + " ");
+                panel.append("<input type='radio' name='question-" + i +
+                 "' value='" + characterQuestions[i].answers[j] + "'>" + characterQuestions[i].answers[j]);
             }
         }
         panel.append("<br>" + "<button id='next'>Next</button>");
-        characterTraits = $('input').val();
-        console.log(characterTraits);
+
 
         $("#next").on("click", function()   {
-            console.log("clicked");
+            console.log("clicked");     
+            
+            hairChosen = $("input[name=question-"+0+"]:checked").val();
+            
+            console.log(hairChosen);
+            
+            ethnicityChosen = $("input[name=question-"+1+"]:checked").val();
+                          
+            console.log(ethnicityChosen);
+
+            genderChosen = $("input[name=question-"+2+"]:checked").val();
+
+            console.log(genderChosen);
+            
+            character = {
+                hairColor: hairChosen,
+                ethnicityChosen: ethnicityChosen,
+                genderChosen: genderChosen,
+            };
+
+            console.log(character);
+
             game.phase1_5();
         });
     },
@@ -50,8 +87,14 @@ var game = {
         panel.html(nar1_5);
 
         panel.append("<br>" + "<button id='next'>Next</button>");
+    },
+
+    phase2:  function() {
+
+        $("#h1tag").html("Phase 2");
+        $("#ptag").append("Highschool");
     }
-}
+};
 
 // Next button needs to dynamically move into next phase of game
 // Use stored values to pull images and disply accordingly 
